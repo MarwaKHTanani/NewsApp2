6ffd61fb974bd7c46d89f75f35fb881d497a5e31
MarwaKHTanani <tananimarwa@gmail.com>
2025-07-17 15:17:21 +0300

    Update: some changes before merge

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const next = document.querySelector(".next");
 const slider = document.getElementById("slider");
 let current = 0;
 
-// const showNewsold = (index) => {
-//   const news = topNews[index];
-//   slider.innerHTML = `
-//     <img src="${news.urlToImage}" alt="${news.title}" class="w-full h-64 object-cover rounded-t-lg"/>
-//     <div class="p-4 bg-[#fdf0d5]">
-//       <h3 class="text-xl font-semibold mb-2 text-gray-900">${news.title}</h3>
-//       <p class="text-gray-700 text-sm">${news.description}</p>
-//     </div>
-//   `;
-// };
-
 const createElement = (tag, classes) => {
   const el = document.createElement(tag);
   classes.forEach((className) => {
@@ -78,17 +67,6 @@ const categoryrender = (category, categoryclass) => {
 
 categoryrender("politics", "politics-news");
 categoryrender("tech", "tech-news");
-//     const card = document.createElement('div');
-//     card.className = 'news-card';
-
-//     card.innerHTML = `
-//         <img src="${news.urlToImage}" alt="${news.title}">
-//         <h4>${news.title}</h4>
-//         <p>${news.description}</p>
-//         `;
-//     classes.appendChild(card);
-//   });
-// };
 
 const blogrender = (blogs, blogclass) => {
   const classes = document.querySelector(`.${blogclass}`);
@@ -110,20 +88,6 @@ const blogrender = (blogs, blogclass) => {
 };
 blogrender(topBlogs, 'blog-news');
 
-//     const card = document.createElement('div');
-//     card.className = 'news-blog';
-
-//     card.innerHTML = `
-//       <img src="${blog.urlToImage}" alt="${blog.title}">
-//       <h4>${blog.title}</h4>
-//       <p>${blog.description}</p>
-//       <button class="read-more">Read More and Give Your Opinion</button>
-//     `;
-//     classes.appendChild(card);
-//   });
-// };
-// blogrender(topBlogs, 'blog-news');
-
 prev.addEventListener("click", () => {
   current = current === 0 ? topNews.length - 1 : current - 1;
   const wrapper = showNews(topNews, current);
@@ -139,5 +103,4 @@ next.addEventListener("click", () => {
 });
 
 const wrapper = showNews(topNews, current);
-
 elementsAppender(slider, [wrapper]);
